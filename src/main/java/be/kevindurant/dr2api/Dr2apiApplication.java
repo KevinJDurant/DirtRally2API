50f14e27bed6082152a53cb3c988a974a6c40d3c
Kevin Durant <kevin.durant@sweetmustard.be>
2021-10-09 09:40:45 +0200

    Add Spring Boot base.

---
+++ src/main/java/be/kevindurant/dr2api/Dr2apiApplication.java
@@ -0,0 +1,13 @@
+package be.kevindurant.dr2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr2apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr2apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/kevindurant/dr2api/Dr2apiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kevindurant.dr2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dr2apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
